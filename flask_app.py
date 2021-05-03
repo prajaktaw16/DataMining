d946dbeacf3dbdcbf7cd46f3c0c3d2c0832eb320
prajakta waikar <prajakta1694@gmail.com>
2021-05-03 07:27:55 +0000

    model

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,22 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+import model
 
 app = Flask(__name__)
+comments = list()
 app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    return render_template("main_page.html")
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # comments.clear()
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    recom = model.predict_class(request.form["contents"])
+    comments.clear()
+    comments.append(recom)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
